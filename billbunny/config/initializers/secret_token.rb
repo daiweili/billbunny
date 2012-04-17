8ff9fdecfbd75a1418ba7e957fd598d8383744ec
Anirudh Todi <anirudhtodi@berkeley.edu>
2012-04-16 19:16:50 -0700

    created billbunny app - using rails version 3.2.3

---
+++ billbunny/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billbunny::Application.config.secret_token = 'c33cf2ea3ea4b6e9ef58bf93c56cfda94429b13be21c2d398a0670fa686dcf9e6fcc3a11d7b0285aff4aad2f62933a8bfa880e556c2ea4f2f0248eb153a8c6e8'
